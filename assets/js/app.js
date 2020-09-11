4666d0b501918d1369b13a7fe1f745597c3c8bce
John Shows <johnshows@Johns-MacBook-Pro-4.local>
2020-09-11 18:37:08 -0400

    intial app.js setup

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+var svgWidth = window.innerWidth;
+var svgHeight = window.innerHeight
+
+var margin = {
+  top: 60,
+  right: 40,
+  bottom: 100,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
